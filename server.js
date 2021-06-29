76d5b2741a07bde0f7b5fa0aa39c02fde3ac2593
aymane <kasbaoui@alphorm.com>
2021-06-29 18:44:43 +0100

    fix 3

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(json())
 // Routes
 app.use('/api/projects', projects)
 app.use('/api/auth', signin)
-app.use('/.well-known/acme-challenge/bbCIkE-Bdm-yqJ0UV2BNu_1eRve-D2G-9f8xaXvgFjE',express.static('./.well-known/acme-challenge/bbCIkE-Bdm-yqJ0UV2BNu_1eRve-D2G-9f8xaXvgFjE'))
-app.get('/.well-known/acme-challenge/MawckB9QvwJA2v8pBLClvKrlGWc8Oka6-0LJniwGSI4',express.static('./.well-known/acme-challenge/MawckB9QvwJA2v8pBLClvKrlGWc8Oka6-0LJniwGSI4'))
+app.use('/.well-known/acme-challenge/bbCIkE-Bdm-yqJ0UV2BNu_1eRve-D2G-9f8xaXvgFjE', (req, res) => res.send('bbCIkE-Bdm-yqJ0UV2BNu_1eRve-D2G-9f8xaXvgFjE.mYBh2FM9DVREjokTDOONyo5p1weWJYCQzNG668rt-70'))
+app.use('/.well-known/acme-challenge/MawckB9QvwJA2v8pBLClvKrlGWc8Oka6-0LJniwGSI4', (req, res) => res.send('MawckB9QvwJA2v8pBLClvKrlGWc8Oka6-0LJniwGSI4.mYBh2FM9DVREjokTDOONyo5p1weWJYCQzNG668rt-70'))
 // React Route
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use('/', express.static(path.join(__dirname, 'client/build')))
 
 const port = process.env.PORT || 5000
 
